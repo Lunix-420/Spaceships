59e160d210e7d84c7e77b8fd80d8ba5c3d0920db
Lunix-420 <davidhess336@gmx.de>
2025-05-14 23:02:48 +0200

    Enemy improvements

---
+++ scripts/enemy.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var player: CharacterBody2D
+
+func _ready():
+	player = get_node("../Player")
+	
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 20000.0 * delta
+	move_and_slide()
